a0eab5dd42eb59e57d22333ed95ece4a86f26fdd
warllei <warllei.s.mrtins@hotmail.com>
2022-12-02 19:55:33 -0300

    correções

--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../context/provider";
 import GetUser from "../hook/getUserHook";
+import { Link } from "react-router-dom";
 import Spinner from "../spinner";
 import Share from "../share";
 import Header from "../header";
 import "./style.scss";
-import { Link } from "react-router-dom";
 
 const Body = () => {
 
@@ -30,7 +30,19 @@ const Body = () => {
             })
     }
 
-    const sendComent = async () => {
+    document.onkeydown = function (e) {
+        if (e.key === 'Escape') {
+            setComents([])
+        }
+    }
+    const sendComent = async (e) => {
+
+
+        e.preventDefault()
+
+        setComentario("")
+
+
         const options = {
             body: JSON.stringify({ userId: localStorage.getItem("id"), comentario: comentario, userNome: localStorage.getItem("nome"), imgUser: localStorage.getItem("userImg"), id_post: localStorage.getItem("post") }),
             method: 'POST',
@@ -39,15 +51,15 @@ const Body = () => {
             },
         };
 
+
         await fetch('http://localhost:3001/novocomentario', options)
 
         ShowComents(localStorage.getItem("post"))
+
     }
 
     const ShowComents = async (event) => {
-
         localStorage.setItem("post", event)
-
         const options = {
             body: JSON.stringify({ id: event }),
             method: 'POST',
@@ -85,23 +97,21 @@ const Body = () => {
         <div id="main">
             <Share />
             <Header />
-            {
-                load
 
-                    ?
-                    <Spinner />
-                    :
-                    false
-            }
+            {load ? <Spinner /> : false}
+
             {
                 coments?.length != 0
                     ?
                     <div className="master">
-                        <span style={{ color: mode == 'ligth' ? '#1c1c1ce0' : '#fff', textAlign: "end", marginTop: 30, cursor: "pointer", width: "96%", fontSize: 50 }} className="material-symbols-outlined close-button-chat" onClick={() => setComents([])}>
-                            close
-                        </span>
+                        <div className="container-close">
+                            <span style={{ cursor: "pointer" }} className="material-symbols-outlined close-button-chat" onClick={() => setComents([])}>
+                                close
+                            </span>
+                        </div>
+
                         <div className="main-chat">
-                            <div className="titulo-comentario" style={{ color: mode == 'ligth' ? '#1c1c1ce0' : '#fff' }}>Comentarios</div>
+                            <div className="titulo-comentario" style={{ color: '#fff', background: mode == 'ligth' ? '#1c1c1c' : '#2a2929' }}>Comentarios</div>
                             <div className="container-chat-content" style={{ background: mode == 'ligth' ? '#f2f2f2' : '#343638' }}>
                                 <div className="chat">
                                     {
@@ -112,19 +122,21 @@ const Body = () => {
                                                         <img style={{ borderRadius: 100 }} src={`data:image/png;base64,${e?.imagem_user_coments}`} />
                                                         <div className="nome" style={{ color: mode == 'ligth' ? '#343638' : '#f2f2f2' }}>{e?.nome}</div>
                                                     </div>
-                                                    <div className="coment" style={{ color: mode == 'ligth' ? '#343638' : '#f2f2f2' }}>{e?.comentario}</div>
+                                                    <div className="coment" style={{ color: '#fff' }}>{e?.comentario}</div>
                                                 </div>
                                             )
                                         })
                                     }
                                 </div>
                             </div>
-                            <div className="send-message-container">
-                                <span style={{ color: '#fff' }} className="material-symbols-outlined" onClick={() => sendComent()}>
-                                    send
-                                </span>
-                                <input placeholder="comentar..." type="text" onChange={(event) => setComentario(event.target.value)} />
-                            </div>
+                            <form className="send-message-container" action={sendComent()} style={{ background: mode == 'ligth' ? '#1c1c1c' : '#2a2929' }}>
+                                <button type="submit" style={{ background: "transparent", border: "none" }} onClick={(e) => sendComent(e)}>
+                                    <div style={{ color: '#fff', fontSize: 40, marginLeft: 10, marginRight: 10 }} className="material-symbols-outlined">
+                                        send
+                                    </div>
+                                </button>
+                                <input placeholder="comentar..." type="text" value={comentario} onChange={(event) => setComentario(event.target.value)} />
+                            </form>
                         </div>
                     </div>
                     :
@@ -191,7 +203,7 @@ const Body = () => {
                     })}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
